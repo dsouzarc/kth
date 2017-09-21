b620910400f7c7d0d5eabd5f53a3c054386f01ee
Ryan D'souza <dsouzarc@gmail.com>
2017-09-21 16:49:59 +0200

    Finished task1

--- Database Technology/lab2/interface.py
+++ Database Technology/lab2/interface.py
@@ -1,89 +1,67 @@
 #!/usr/bin/python
 
-# Lines that start as this one does are comments and have no effect on
-# the execution of the program.  (Most added by John Folkesson to help
-# understand the code.)
-
-# When you import the pgdb  module, all the
-# classes and functions in that module become available for you to
-# use.  For example you can now use the pgdb.connect() function to
-# establish a connection to your copy of the database. 
-
-# Possibly helpful example code is found at:
-# https://wiki.inf.ed.ac.uk/twiki/pub/DatabaseGroup/TeachingPythonPostgreGuide/dbexample.py.txt
-
 import pgdb
 from sys import argv
 
-# we define a class that we will use in our main program
-class DBContext:
-    """DBContext is a small interface to a database that simplifies SQL.
-    Each function gathers the minimal amount of information required and executes the query."""
 
-# we first define the class, its definitions, functions attributes,
-# members ... what ever you like to call this stuff.  Then way down at
-# the bottom of this file we will create on of these.
-# this __init___ operation is automatically called when the object is created.
+class DBContext:
+
+    """ DBContext is a small interface to a database that simplifies SQL.
+        Each function gathers the minimal amount of information required and executes the query
+    """
+
 
     def __init__(self): #PG-connection setup
         print("AUTHORS NOTE: If you submit faulty information here, I am not responsible for the consequences.")
 
-# we need to call the connect function with the right parameters some
-# of wheich we 'hard code here such as the host, wnd others we call
-# the built in python function raw_input to get from the user.  All are stored in a variable that we chose to call params.
-        params = {'host':'nestor2.csc.kth.se', 'user':raw_input("Username: "), 'database':raw_input("Database: "), 'password':raw_input("Password: ")}
-        self.conn = pgdb.connect(**params)
-        # Here we create an attribute of our class (DBContex) called
-        # menu as a list of strings.
+        #TODO: Remove hardcoding
+        params = {
+                'host':'nestor2.csc.kth.se', 
+                'user': '', #raw_input("Username: "), 
+                'database': '', #raw_input("Database: "), 
+                'password': '' #raw_input("Password: ")
+        }
+
         self.menu = ["Select.", "Insert.", "Remove.", "Exit"]
-        # Here we create a cursor (see chap 9) and
-        # http://www.python.org/dev/peps/pep-0249/
+
+        self.conn = pgdb.connect(**params)
         self.cur = self.conn.cursor()
 
-# Here we define a member function that we can later call repeatidly
+
     def print_menu(self):
-        """Prints a menu of all functions this program offers.  Returns the numerical correspondant of the choice made."""
+        """ Prints a menu of all functions this program offers.  
+                Returns the numerical correspondant of the choice made
+        """
+
         for i,x in enumerate(self.menu):
             print("%i. %s"%(i+1,x))
-            # this get_int function is defined below
+
         return self.get_int()
 
-    def get_int(self):
-        """Retrieves an integer from the user.
-        If the user fails to submit an integer, it will reprompt until an integer is submitted."""
-        while True:
-            # we go round here untill we get to return (ie while True)
 
-          #  The try statement works as follows.  First, the try
-          #  clause (the statement(s) between the try and except
-          #  keywords) is executed. If no exception occurs, the except
-          #  clause is skipped and execution of the try statement is
-          #  finished. If an exception occurs during execution of the
-          #  try clause, the rest of the clause is skipped. Then if
-          #  its type matches the exception named after the except
-          #  keyword, the except clause is executed, and then
-          #  execution continues after the try statement.  If an
-          #  exception occurs which does not match the exception named
-          #  in the except clause, it is passed on to outer try
-          #  statements; if no handler is found, it is an unhandled
-          #  exception and execution stops with a message as shown
-          #  above.
+    def get_int(self):
+        """ Retrieves an integer from the user.
+            If the user fails to submit an integer, it will reprompt until an integer is submitted
+        """
+
+        while True:
+
             try:
-                # an Error here (ie wrong input type) jumps to except
                 choice = int(input("Choose: "))
                 if 1 <= choice <= len(self.menu):
                     return choice
-                # here we had a number but it was out of range
                 print("Invalid choice.")
+
             except (NameError,ValueError, TypeError, SyntaxError):
                 print("That was not a number, genious.... :(")
 
-                # This function will be called if the user choses select.
+
     def select(self):
-        """Finds and prints tuples.
-        Will query the user for the information required to identify a tuple.
-        If the filter field is left blank, no filter will be used."""
-        # raw_input returns the entire line entered at the promt. The
+        """ Finds and prints tuples.
+            Will query the user for the information required to identify a tuple.
+            If the filter field is left blank, no filter will be used
+            Written by the Professor
+        """
         # split(",") method then creates a list of the relations
         # (tables) that you have separated by commas.  The strip
         # method just remove the white space.  So this line is read
@@ -95,16 +73,21 @@ class DBContext:
 
         tables = [x.strip() + " natural join " for x in raw_input("Choose table(s): ").split(",")]
 
-# Here we do some char pointer tricks to remove the extra " natural
-# join " (14 characters
+        # Here we do some char pointer tricks to remove the extra " natural
+        # join " (14 characters
         tables[len(tables)-1] = tables[len(tables)-1][0:len(tables[len(tables)-1])-14]
-# pring the result to the screen
+
+        # print the result to the screen
         print tables
-# here columns becomes the string that you type at prompt for Choose columns.
+        # here columns becomes the string that you type at prompt for Choose columns.
+
         columns = raw_input("Choose column(s): ")
         print columns
+
         #list comprehension building a list ready to be reduced into a string.
         filters = raw_input("Apply filters: ")
+
+
         # This will set query to the long string "SELECT columns FROM
         # tables WHERE filters;" The %s indicate that a string from a
         # variable will be inserted here, Those string variables
@@ -120,50 +103,166 @@ class DBContext:
         except (NameError,ValueError, TypeError,SyntaxError):
             print "  Bad input."
             return
+
         print(query)
-        # Here we do the select query at the cursor
-        # No errors are caught so this crashes horribly on malformed queries
+
+        # Execute the query, print results- No error catching so crashes on malformed queries
         self.cur.execute(query)       
-        # This function is defined below
         self.print_answer()
-        #OK now you do the next two:
+
+
+    def get_tables_and_columns(self):
+        """
+            Returns a dictionary with key: table_name, value: set of columns in that table
+            Created by Ryan D'souza
+
+            :returns dict: all column names (values) mapped to their table names (key)
+        """
+
+        table_columns = {}
+
+        query = ("SELECT table_name, column_name "
+                    "FROM INFORMATION_SCHEMA.COLUMNS "
+                    "WHERE table_schema='public'; ")
+        self.cur.execute(query)
+
+        result_table_values = self.cur.fetchall()
+
+        for result_table_value in result_table_values:
+            table_name = result_table_value[0]
+            column_name = result_table_value[1]
+
+            if not table_name in table_columns:
+                table_columns[table_name] = set()
+
+            table_columns[table_name].add(column_name)
+
+        return table_columns
+
+
     def remove(self):
-        """Removes tuples.
-        Will query the user for the information required to identify a tuple.
-        If the filter field is left blank, no filters will be used."""
-        pass
+        """
+            Removes tuples.
+            Will query the user for the information required to identify a tuple.
+            If the filter field is left blank, no filters will be used
+        """
+
+        table_columns = self.get_tables_and_columns()
+
+        #Prompt user for table name - check for validity
+        delete_table_name = raw_input("Choose table to delete from: ")
+        if delete_table_name not in table_columns:
+            raise pgdb.DatabaseError("'%s' not in database" % delete_table_name)
+
+        
+        delete_conditions = raw_input("Specify DELETE conditions: ")
+
+        delete_command = ""
+
+        #Delete all rows from this table
+        if delete_conditions is None or len(delete_conditions) == 0 or delete_conditions == "*":
+                delete_command = "DELETE * FROM {table_name}".format(table_name=delete_table_name)
+
+        #We only delete specific rows based on a condition
+        else:
+            delete_command = ("DELETE FROM {table_name} WHERE {conditions}"
+                                .format(table_name=delete_table_name,
+                                            conditions=delete_conditions))
+
+        print(delete_command)
+
+        #Execute and save
+        self.cur.execute(delete_command)
+        self.conn.commit()
+
 
     def insert(self):
-        """inserts tuples.
-        Will query the user for the information required to create tuples."""
-        pass    
+        """
+            Inserts tuples.
+            Will query the user for the information required to create tuples
+        """
+
+        table_columns = self.get_tables_and_columns()
+
+        #Prompt user for the table name. Check to see if it is a valid table name
+        insert_table_name = raw_input("Choose table to insert into: ")
+        if insert_table_name not in table_columns:
+            raise pgdb.DatabaseError("'%s' not in database" % insert_table_name)
+
+        
+        insert_column_names = raw_input("Choose column name(s): ").replace(" ", "").split(",")
+        column_names_string = ""
+
+        #Formatting for if we're specifying the column names 
+        if len(insert_column_names) > 0 and len(insert_column_names[0]) > 0:
+            column_names_string = " ("
+
+            #Append every valid column name to our string
+            for insert_column_name in insert_column_names:
+                if insert_column_name in table_columns[insert_table_name]:
+                    column_names_string += insert_column_name + ", "
+                else:
+                    raise pgdb.DatabaseError("'%s' not found in table '%s'" % (insert_column_name,
+                                                insert_table_name))
+                                                
+                    
+            #Get rid of the trailing comma and space, finish insert formatting
+            column_names_string = column_names_string[:-2] + ")"
+
+
+        #Start formatting the actual values themselves
+        insert_values = raw_input("Insert value(s): ").replace(" ", "").split(",")
+        insert_values_string = "("
+
+        for insert_value in insert_values:
+            insert_values_string += insert_value + ", "
+
+        #Get rid of the trailing comma and space if we had values
+        if len(insert_values) > 0:
+            insert_values_string = insert_values_string[:-2]
+
+        insert_values_string += ")"
+
+        #Full INSERT command
+        insert_command = ("INSERT INTO {table_name} {column_names} VALUES {values}"
+                            .format(table_name=insert_table_name,
+                                        column_names=column_names_string,
+                                        values=insert_values_string))
+
+        print(insert_command)
+
+        #Execute and save
+        self.cur.execute(insert_command)       
+        self.conn.commit()
+
+
     def exit(self):    
+        """ Close our database """
+
         self.cur.close()
         self.conn.close()
         exit()
     
-    def print_answer(self):
-# We print all the stuff that was just fetched.
-            print("\n".join([", ".join([str(a) for a in x]) for x in self.cur.fetchall()]))
 
-    # we call this below in the main function.
+    def print_answer(self):
+        """ We print all the stuff that was just fetched. """
+
+        print("\n".join([", ".join([str(a) for a in x]) for x in self.cur.fetchall()]))
+
+
     def run(self):
-        """Main loop.
-        Will divert control through the DBContext as dictated by the user."""
+        """Main loop. Will divert control through the DBContext as dictated by the user."""
+
         actions = [self.select, self.insert, self.remove, self.exit]
         while True:
             try:
-                # So this is executed right to left, The print_menu
-                # function is run first (defined above), then the
-                # return value is used as an index into the list
-                # actions defined above, then that action is called.
                 actions[self.print_menu()-1]()
                 print
             except IndexError:
-# if somehow the index into actions is wrong we just loop back
+                # if somehow the index into actions is wrong we just loop back
                 print("Bad choice")
 
-# This strange looking line is what kicks it all off.  So python reads until it sees this then starts executing what comes after-
+
 if __name__ == "__main__":
     db = DBContext()
     db.run()
